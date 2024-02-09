4f9d0b861da9a33ef7c44655dd762249f4cf0f86
j2noo <wlsdndml213@gmail.com>
2024-02-09 13:16:43 +0900

    feat : 초기 세lㅇ

---
+++ src/main/java/com/backend/kutorial/KutorialApplication.java
@@ -0,0 +1,13 @@
+package com.backend.kutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/kutorial/KutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.kutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
