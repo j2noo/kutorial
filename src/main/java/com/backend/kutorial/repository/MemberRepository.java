224983f7cef589e4a8af91c80af71cedf6bea3be
j2noo <wlsdndml213@gmail.com>
2024-02-21 03:25:00 +0900

    feat :ã Member 레포지토리 생성

---
+++ src/main/java/com/backend/kutorial/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.backend.kutorial.repository;
+
+import com.backend.kutorial.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
+}
+
