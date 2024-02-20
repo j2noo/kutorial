17428a3ea08ab5221bb57bbc95bb82cd796ba713
j2noo <wlsdndml213@gmail.com>
2024-02-21 03:00:41 +0900

    feat : Member 도메인 생성

---
+++ src/main/java/com/backend/kutorial/domain/Member.java
@@ -0,0 +1,34 @@
+package com.backend.kutorial.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String loginId;
+    private String nickname;
+    private String password;
+    private Integer age;
+
+    @Builder
+    public Member(Long id, String loginId, String nickname, String password, Integer age) {
+        this.id = id;
+        this.loginId = loginId;
+        this.nickname = nickname;
+        this.password = password;
+        this.age = age;
+    }
+
+    protected Member() {
+
+    }
+}
